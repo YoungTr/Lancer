a86f98e77ba3cf70a967bd02b3ca2e20f7f67640
codelang <794149142@qq.com>
2022-06-21 09:52:35 +0800

    mod:更改日志顺序

--- app/src/main/java/com/bomber/strace/core/SysTracer.java
+++ app/src/main/java/com/bomber/strace/core/SysTracer.java
@@ -6,20 +6,19 @@ import android.util.Log;
 import java.util.Stack;
 
 public class SysTracer {
-    private static Stack<String> methodStack = new Stack<>();
+    private static final Stack<String> methodStack = new Stack<>();
     /**
      * 方法调用栈深度
      */
-    private static int maxSize = 6;
+    private static final int maxSize = 6;
 
     public static void i(String className, String methodName) {
         String sectionName = className + "." + methodName;
         if (methodStack.size() <= maxSize) {
             methodStack.push(sectionName);
             Trace.beginSection(sectionName);
+            Log.i("TAG", "SysTracer.i = " + sectionName);
         }
-
-        Log.i("TAG", "SysTracer.i = " + sectionName);
     }
 
     public static void o(String className, String methodName) {
@@ -28,9 +27,8 @@ public class SysTracer {
         if (methodStack.size() > 0) {
             methodStack.pop();
             Trace.endSection();
+            Log.i("TAG", "SysTracer.o = " + sectionName);
         }
-
-        Log.i("TAG", "SysTracer.o = " + sectionName);
     }
 
     public static void catchIn(String className, String methodName) {
